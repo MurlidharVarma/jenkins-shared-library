1d12ee2aeb7c440db6bf08da2ccab137d350d58d
Murlidhar Varma <murlidhar.varma@gmail.com>
2023-11-13 16:08:45 +0530

    test

--- vars/greet.groovy
+++ vars/greet.groovy
@@ -2,11 +2,11 @@
 // def newPath = path.split("var")[0]+"/resources/lib"
 // this.class.classLoader.rootLoader.addURL(new URL(newPath+"/edgegrid-signer-core-5.0.0.jar"))
 
-def akcore = libraryResource 'lib/edgegrid-signer-core-5.0.0.jar';
-
 import com.aipeel.jenkins.HelloWorld
 
 def call(String msg = "Hello World"){
+    def akcore = libraryResource 'lib/edgegrid-signer-core-5.0.0.jar';
+    echo(akcore);
     HelloWorld obj = new HelloWorld(akcore);
     echo("FROM GREET: "+ obj.sayHello(msg));
 }
