97cd7c80e4b2d2779174eb29bf8a7040afd26f73
Murlidhar Varma <murlidhar.varma@gmail.com>
2023-11-13 16:03:13 +0530

    test

--- src/com/aipeel/jenkins/HelloWorld.groovy
+++ src/com/aipeel/jenkins/HelloWorld.groovy
@@ -2,7 +2,7 @@ package com.aipeel.jenkins;
 
 
 // @Grab('com.akamai.edgegrid:edgegrid-signer-core:5.0.0')
-import com.akamai.edgegrid.signer.NullSafeComparator;
+// import com.akamai.edgegrid.signer.NullSafeComparator;
 
 /**
  * Hello world!
@@ -12,6 +12,7 @@ public class HelloWorld
 {
 
     def String sayHello(){
+        echo akcore
         akcore.NullSafeComparator.new();
         return "Hello World from return";
     }
